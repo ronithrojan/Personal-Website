28857aded0850d35b384247355eeec5b87862f8a
Roniya Rojan <roniyarojan24@gmail.com>
2020-06-03 11:40:36 -0400

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AboutComponent } from "./about/about.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "homepage", component: HomepageComponent },
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "", redirectTo: "/homepage", pathMatch: "full" },
+  { path: "**", redirectTo: "/homepage", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
